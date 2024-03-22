4f415f0cce98531a81d8538c0a592d7438a5ee1f
ibra-kdbra <git-repo-develop@outlook.com>
2024-03-22 23:57:46 +0300

    source start

---
+++ src/pages/_app.tsx
@@ -0,0 +1,18 @@
+import "@/styles/globals.css";
+import Head from "next/head";
+import type { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <>
+  <Head>
+				<title>LeetClone</title>
+				<meta name='viewport' content='width=device-width, initial-scale=1' />
+				<link rel='icon' href='/favicon.png' />
+				<meta
+					name='description'
+					content='Web application that contains leetcode problems and video solutions'
+				/>
+			</Head>
+      <Component {...pageProps}/>
+  </>;
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+
+export default function Home() {
+  return (
+    <main
+      className={``}
+    >
+      
+    </main>
+  );
+}

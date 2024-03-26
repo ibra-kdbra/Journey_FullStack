bcf00ba03d47d0272f3f4322bb6cea123bc75c20
ibra-kdbra <git-repo-develop@outlook.com>
2024-03-27 00:00:36 +0300

    main index and app page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import "@/styles/globals.css";
-import Head from "next/head";
 import type { AppProps } from "next/app";
-
+import Head from "next/head";
+import { RecoilRoot } from "recoil";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 export default function App({ Component, pageProps }: AppProps) {
-  return <>
-  <Head>
+	return (
+		<RecoilRoot>
+			<Head>
 				<title>LeetClone</title>
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<link rel='icon' href='/favicon.png' />
@@ -13,6 +16,8 @@ export default function App({ Component, pageProps }: AppProps) {
 					content='Web application that contains leetcode problems and video solutions'
 				/>
 			</Head>
-      <Component {...pageProps}/>
-  </>;
+			<ToastContainer />
+			<Component {...pageProps} />
+		</RecoilRoot>
+	);
 }

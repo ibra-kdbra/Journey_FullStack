6316c552e7ddf6cf0aae2a588917cdba9154ebc7
ibra-kdbra <git-repo-develop@outlook.com>
2024-04-12 22:55:58 +0300

    update APP for advanced SOLID princples

--- react-s.o.l.i.d/src/App.tsx
+++ react-s.o.l.i.d/src/App.tsx
@@ -1,17 +1,33 @@
 import "./App.css";
-import { DIP } from "./principles/DIP";
-import { LSP } from "./principles/LSP";
-import { OCP } from "./principles/OCP";
 import { SRP } from "./principles/srp";
+import { OCP } from "./principles/OCP";
+import { LSP } from "./principles/LSP";
+import { ISP } from "./principles/ISP";
+import { DIP } from "./principles/DIP";
 
+import { SRP_ADVANCED } from "./advanced/SRP";
+import { OCP_ADVANCED } from "./advanced/OCP";
+import { LSP_ADVANCED } from "./advanced/LSP";
+import { ISP_ADVANCED } from "./advanced/ISP";
+import { DIP_ADVANCED } from "./advanced/DIP";
 
 function App() {
   return (
     <div className="flex min-w-full h-full justify-center items-center p-8">
-      <SRP />
+      {/* simple SOLID princples */}
+      {/* <SRP /> */}
       {/* <OCP /> */}
       {/* <LSP /> */}
+      {/* <ISP /> */}
       {/* <DIP /> */}
+
+
+      {/* advanced SOLIc principles */}
+      {/* <SRP_ADVANCED/> */}
+      {/* <OCP_ADVANCED/> */}
+      {/* <LSP_ADVANCED/> */}
+      {/* <ISP_ADVANCED/> */}
+      {/* <DIP_ADVANCED/> */}
     </div>
   );
 }

8c395279537647e9ae6098ae88c1882a514335da
ibra-kdbra <git-repo-develop@outlook.com>
2024-04-15 13:06:31 +0300

    config,deps,packages file

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from "drizzle-kit";
+import dotenv from "dotenv"
+export default {
+  schema: "./src/models/db/schema.ts",
+  out: "./src/models/db/migrations",
+  driver: "turso",
+  dbCredentials: {
+    url: process.env.TURSO_DB_URL as string,
+    authToken: process.env.TURSO_DB_AUTH_TOKEN as string,
+  },
+} satisfies Config;

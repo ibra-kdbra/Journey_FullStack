17f1ed9831a9acc2f6192c597fb11c28a614a228
ibra-kdbra <git-repo-develop@outlook.com>
2024-04-24 20:25:47 +0300

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-
+import Nav from "./components/Nav";
+import Jumbotron from "./components/Jumbotron";
+import SoundSection from "./components/SoundSection";
+import DisplaySection from "./components/DisplaySection";
+import WebgiViewer from "./components/WebgiViewer";
+import Loader from "./components/Loader";
+import { useRef } from "react";
 function App() {
-
+  const WebgiViewerRef = useRef();
+  const contentRef = useRef();
+  
+  const handlePreview = () => {
+    WebgiViewerRef.current.triggerPreview();
+  };
   return (
     <div className="App">
-      Hello
+      <Loader/>
+      <div ref={contentRef} id="content">
+      <Nav />
+      <Jumbotron />
+      <SoundSection />
+      <DisplaySection triggerPreview={handlePreview} />
+      </div>
+      <WebgiViewer contentRef={contentRef} ref={WebgiViewerRef} />
     </div>
   );
 }

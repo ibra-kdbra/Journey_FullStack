dd1830998160ac25104d25b52580ae23e5717d7e
ibra-kdbra <git-repo-develop@outlook.com>
2024-04-24 20:24:55 +0300

    within the third third section

---
+++ src/components/SoundSection.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+function SoundSection() {
+
+  const handleLearnMore = () => {
+    const element =  document.querySelector('.display-section'); 
+    window.scrollTo({
+      top: element?.getBoundingClientRect().bottom,
+      left:0 , 
+      behavior:'smooth'
+    })
+  }
+  return (
+    <div className="sound-section wrapper">
+      <div className="body">
+        <div className="sound-section-content content">
+          <h2 className="title">New sound system</h2>
+          <p className="text">Feel the base</p>
+          <span className="description">
+            Don't miss out any beat.
+          </span>
+          <ul className="links">
+            <li>
+                <button className="button">
+                    buy
+                </button>
+                
+            </li>
+            <li>
+                <a className="link" onClick={handleLearnMore}>
+                   learn more 
+                </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SoundSection;

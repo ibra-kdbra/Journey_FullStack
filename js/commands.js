8a3f9a984f7dabd11de6a6a20eb1afb459e3e003
ibra-kdbra <git-repo-develop@outlook.com>
2024-05-21 14:54:25 +0300

    common questions, caret use, main logic

---
+++ js/commands.js
@@ -0,0 +1,112 @@
+var dev = "https://dev.to/ibra-kdbra";
+var linkedin = "https://www.linkedin.com//";
+var telegram = "https://t.me/Ibra_22kh";
+var github = "https://github.com/ibra-kdbra";
+var sudo = "https://www.youtube.com/watch?v=dQw4w9WgXcQ?autoplay=1";
+var email = "sonarfarouq@gmail.com";
+
+aboutme = [
+  "<br>",
+  "<span class='underline'>Hey, I'm ibra!👋</span>",
+  "<br>",
+  "<li>🔭 I’m currently a 5th year undergrad, pursuing BSc.</li>",
+  "<li>🌱 My fields of intetest are</li>",
+  `<pre class="whitespace-pre-wrap">
+        - Fullstack Development
+        - UI/UX Designing</pre>`,
+  `<li><pre class="whitespace-pre-wrap">👯 I would love to work with anyone who wants to build a 
+   professional website for their business or even a personal 
+   website to showcase their portfolio.
+   I love learning new stuff and using it to improve my skills.
+   I primarily use NextJS and Tailwind CSS when building my 
+   websites (projects).
+   You can learn more about the projects that I have built using 
+   the project command.
+   You can browse around my website to find out more about 
+   me.
+   *Try using a social command.`,
+  `<li><pre class="whitespace-pre-wrap">👨‍💻 Check out my github profile to view my projects
+   *Use the github command to take a look at my GitHub profile.`,
+  `<li>📫 Contact me at <a href= "https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=sonarfarouq@gmail.com">sonarfarouq@gmail.com</a>`,
+  "<br>",
+];
+
+social = [
+  "<br>",
+  'dev        <a href="' +
+    dev +
+    '" target="blank">dev.to/ibra-kdbra' +
+    "</a>",
+  'linkedin       <a href="' +
+    linkedin +
+    '" target="blank">linkedin/' +
+    "</a>",
+  'instagram      <a href="' +
+    telegram, +
+    '" target="blank">t.me/Ibra_22khe' +
+    "</a>",
+  'github         <a href="' +
+    github +
+    '" target="blank">github/ibra-kdbra' +
+    "</a>",
+  
+  "<br>",
+];
+
+projects = [
+  "<br>",
+  `<a href="https://github.com/ibra-kdbra/Journey_FullStack/tree/glider_transition" target="blank"><span class='underline'>Glider Transition</span>`,
+  "<br>",
+  `<pre class="indent-8 whitespace-pre-wrap">A functional glider transition for the web, easy motion animation. 
+Project is live here <a href="https://transition-glider.netlify.app/" target="blank">https://transition-glider.netlify.app/</a>
+*Please do not overuse the link.
+
+Built With
+
+    *React
+    *Tailwind CSS
+    *Frame-Motion
+    *Npm
+`,
+
+  "<br>",
+
+  `<a href="https://github.com/ibra-kdbra/Journey_FullStack/tree/leetCode" target="blank"><span class='underline'>LeetCode  Clone</span>`,
+  "<br>",
+  `<pre class="indent-8 whitespace-pre-wrap">Leet Code Problem Solving with JS language, many data structers problems to  solve, solution after many errors, and the hint you cant find in the nomral LeetCode website
+Project is live here <a href="https://leetcode-copy.netlify.app/" target="blank">https://leetcode-copy.netlify.app/</a>
+
+Built With
+
+    *Next.js
+    *Tailwind CSS
+    *React
+    *Firebase
+    *TypeScript
+`,
+
+  "<br>",
+];
+
+help = [
+  `<br><pre class="whitespace-pre-wrap">
+  <span class="command">aboutme</span>    
+   <span class="command">↳</span>&nbsp;Diplays Who I am?
+  <span class="command">social</span>     
+   <span class="command">↳</span>&nbsp;Lists social networks.
+  <span class="command">projects</span>   
+   <span class="command">↳</span>&nbsp;View coding projects.
+  <span class="command">email</span>      
+   <span class="command">↳</span>&nbsp;To send me a email.
+  <span class="command">history</span>    
+   <span class="command">↳</span>&nbsp;View command history.
+  <span class="command">help</span>       
+   <span class="command">↳</span>&nbsp;Displays this help msg.
+  <span class="command">sudo</span>       
+   <span class="command">↳</span>&nbsp;Try it out for yourself.
+  <span class="command">clear</span>
+   <span class="command">↳</span>&nbsp;Clear terminal.</pre><br>`,
+];
+banner = [
+  `<div class="pt-2"><span class="text-[#7d82d7db] ">Welcome to my portfolio! — Type <span class="command text-[#75e1e7]">help</span> for a list of supported commands.</span></div>`,
+];

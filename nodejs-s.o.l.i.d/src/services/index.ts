1a07814e5b02f9f7defbf6ebae264a415b062d32
ibra-kdbra <git-repo-develop@outlook.com>
2024-06-03 14:18:10 +0300

    export services

---
+++ nodejs-s.o.l.i.d/src/services/index.ts
@@ -0,0 +1,11 @@
+import exportData from "./ExportFile";
+import { hashPassword } from "./HashPassword";
+import payService from "./PayService";
+import { sendEmail } from "./SendEmails";
+
+export {
+    exportData,
+    hashPassword,
+    payService,
+    sendEmail
+}

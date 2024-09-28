ff9af29fdc842c964c8580ec927741408896f36c
ibra-kdbra <git-repo-develop@outlook.com>
2024-09-28 13:42:20 +0300

    meta data utilities

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,55 +1,56 @@
-import { type ClassValue, clsx } from 'clsx'
-import { Metadata } from 'next'
-import { twMerge } from 'tailwind-merge'
-
+import { type ClassValue, clsx } from "clsx"
+import { Metadata } from "next"
+import { twMerge } from "tailwind-merge"
+ 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function absoluteUrl(path: string) {
-  if (typeof window !== 'undefined') return path
-  if (process.env.VERCEL_URL)
+export function absoluteUrl(path: string){
+  if(typeof window !== 'undefined'){
+    return path
+  }
+  if(process.env.VERCEL_URL){
     return `https://${process.env.VERCEL_URL}${path}`
-  return `http://localhost:${
-    process.env.PORT ?? 3000
-  }${path}`
+  }
+  return `http://localhost:${process.env.PORT ?? 3000}${path}`
 }
 
 export function constructMetadata({
-  title = "Quill - the SaaS for students",
-  description = "Quill is an open-source software to make chatting to your PDF files easy.",
-  image = "/thumbnail.png",
-  icons = "/favicon.ico",
-  noIndex = false
-}: {
-  title?: string
-  description?: string
-  image?: string
-  icons?: string
-  noIndex?: boolean
-} = {}): Metadata {
-  return {
+  title= "Parleen - the Saas for documents",
+  description= "Parleen is an open-source software to help you chat with your PDFs.",
+  image= "/thumbnail.png",
+  icons= "/favicon.ico",
+  noIndex= false
+}:{
+  title?: string,
+  description?: string,
+  image?: string,
+  icons?:string,
+  noIndex?:boolean
+} = {}) : Metadata {
+  return{
     title,
     description,
     openGraph: {
       title,
       description,
-      images: [
+      images:[
         {
           url: image
         }
       ]
     },
-    twitter: {
+    twitter:{
       card: "summary_large_image",
       title,
       description,
-      images: [image],
-      creator: "@joshtriedcoding"
+      images:[image],
+      creator: "@MdFaizan711"
     },
     icons,
-    metadataBase: new URL('https://quill-jet.vercel.app'),
-    themeColor: '#FFF',
+    metadataBase:new URL('https://pdf-insight-fa.vercel.app'),
+    themeColor: "#fff",
     ...(noIndex && {
       robots: {
         index: false,

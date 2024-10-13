5ebc7655e2260a8c5cecca062ed0570a5798eed2
ibra-kdbra <git-repo-develop@outlook.com>
2024-10-14 00:41:36 +0300

    updated format

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,16 +1,13 @@
-'use client'
+"use client";
 
-import { trpc } from '@/app/_trpc/client'
-import { absoluteUrl } from '@/lib/utils'
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
-import { httpBatchLink } from '@trpc/client'
-import { PropsWithChildren, useState } from 'react'
+import { PropsWithChildren, useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { trpc } from "@/app/_trpc/client";
+import { httpBatchLink } from "@trpc/client";
+import { absoluteUrl } from "@/lib/utils";
 
 const Providers = ({ children }: PropsWithChildren) => {
-  const [queryClient] = useState(() => new QueryClient())
+  const [queryClient] = useState(() => new QueryClient());
   const [trpcClient] = useState(() =>
     trpc.createClient({
       links: [
@@ -19,17 +16,15 @@ const Providers = ({ children }: PropsWithChildren) => {
         }),
       ],
     })
-  )
+  );
 
   return (
-    <trpc.Provider
-      client={trpcClient}
-      queryClient={queryClient}>
-      <QueryClientProvider client={queryClient}>
-        {children}
-      </QueryClientProvider>
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+        <QueryClientProvider client={queryClient}>
+            {children}    
+        </QueryClientProvider>
     </trpc.Provider>
-  )
-}
+  );
+};
 
-export default Providers
+export default Providers;

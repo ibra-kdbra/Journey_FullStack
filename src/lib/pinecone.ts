cc9b02c1ba281cdfb420572bab55076faff1bda7
ibra-kdbra <git-repo-develop@outlook.com>
2024-10-14 00:40:05 +0300

    finished third libraries

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -1,12 +1,6 @@
-import { PineconeClient } from '@pinecone-database/pinecone'
+import {Pinecone} from '@pinecone-database/pinecone'
 
-export const getPineconeClient = async () => {
-  const client = new PineconeClient()
-
-  await client.init({
+export const pinecone = new Pinecone({
     apiKey: process.env.PINECONE_API_KEY!,
     environment: 'us-east1-gcp',
-  })
-
-  return client
-}
+})
--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -4,15 +4,15 @@ import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import Stripe from 'stripe'
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? '', {
-  apiVersion: '2023-08-16',
+  apiVersion: '2023-10-16',
   typescript: true,
 })
 
 export async function getUserSubscriptionPlan() {
   const { getUser } = getKindeServerSession()
-  const user = getUser()
+  const user = await getUser()
 
-  if (!user.id) {
+  if (!user || !user.id) {
     return {
       ...PLANS[0],
       isSubscribed: false,

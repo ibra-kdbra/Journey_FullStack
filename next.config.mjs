29b4053e2e3767bbe1fefe045b4f6be877d9d672
ibra-kdbra <git-repo-develop@outlook.com>
2024-10-20 15:44:48 +0300

    packages, deps, providers, configurations

---
+++ next.config.mjs
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: false,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'gravatar.com',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'avatars.githubusercontent.com',
+                pathname: '/**',
+            },
+        ],
+    },
+};
+
+export default nextConfig;

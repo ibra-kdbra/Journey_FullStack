cd45d709a1462b76b93e4c9c48704b6c55654ffb
ibra-kdbra <git-repo-develop@outlook.com>
2024-10-20 16:17:58 +0300

    ignored the wanring

--- app/(routes)/workspace/_components/Editor.tsx
+++ app/(routes)/workspace/_components/Editor.tsx
@@ -1,12 +1,10 @@
 "use client"
 import React, { useEffect, useRef, useState } from 'react'
-import EditorJS from '@editorjs/editorjs';
-// @ts-ignore
+import EditorJS, { OutputData } from '@editorjs/editorjs';
 import Header from '@editorjs/header';
-// @ts-ignore
 import List from "@editorjs/list";
 // @ts-ignore
-import Checklist from '@editorjs/checklist'
+import Checklist from '@editorjs/checklist';
 // @ts-ignore
 import Paragraph from '@editorjs/paragraph';
 // @ts-ignore
@@ -16,98 +14,105 @@ import { api } from '@/convex/_generated/api';
 import { toast } from 'sonner';
 import { FILE } from '../../dashboard/_components/FileList';
 
-const rawDocument={
-    "time" : 1550476186479,
-    "blocks" : [{
-        data:{
-            text:'Document Name',
-            level:2
+const rawDocument = {
+    "time": 1550476186479,
+    "blocks": [{
+        data: {
+            text: 'Document Name',
+            level: 2
         },
-        id:"123",
-        type:'header'
+        id: "123",
+        type: 'header'
     },
     {
-        data:{
-            level:4
+        data: {
+            level: 4
         },
-        id:"1234",
-        type:'header'
+        id: "1234",
+        type: 'header'
     }],
-    "version" : "2.8.1"
+    "version": "2.8.1"
 }
-function Editor({onSaveTrigger,fileId,fileData}:{onSaveTrigger:any,fileId:any,fileData:FILE}) {
-    const ref=useRef<EditorJS>();
-    const updateDocument=useMutation(api.files.updateDocument);
-    const [document,setDocument]=useState(rawDocument);
-    useEffect(()=>{
-        fileData&&initEditor();
-    },[fileData])
 
-    useEffect(()=>{
-      console.log("triiger Value:",onSaveTrigger);
-      onSaveTrigger&&onSaveDocument();
-    },[onSaveTrigger])
+function Editor({ onSaveTrigger, fileId, fileData }: { onSaveTrigger: any, fileId: any, fileData: FILE }) {
+    const ref = useRef<EditorJS>();
+    const updateDocument = useMutation(api.files.updateDocument);
+    const [document, setDocument] = useState(rawDocument);
 
-    const initEditor=()=>{
+    useEffect(() => {
+        if (fileData) {
+            initEditor();
+        }
+    }, [fileData]);
+
+    useEffect(() => {
+        console.log("trigger Value:", onSaveTrigger);
+        if (onSaveTrigger) {
+            onSaveDocument();
+        }
+    }, [onSaveTrigger]);
+
+    const initEditor = () => {
         const editor = new EditorJS({
-            /**
-             * Id of Element that should contain Editor instance
-             */
-
-            tools:{
+            holder: 'editorjs',
+            tools: {
                 header: {
+                  // @ts-ignore
                     class: Header,
                     shortcut: 'CMD+SHIFT+H',
-                    config:{
-                        placeholder:'Enter a Header'
+                    config: {
+                        placeholder: 'Enter a Header'
                     }
-                  },
-                  list: {
+                },
+                list: {
+                  // @ts-ignore
                     class: List,
                     inlineToolbar: true,
                     config: {
-                      defaultStyle: 'unordered'
+                        defaultStyle: 'unordered'
                     }
-                  },
-                  checklist: {
+                },
+                checklist: {
                     class: Checklist,
                     inlineToolbar: true,
-                  },
-                  paragraph: Paragraph,
-                  warning: Warning,
+                },
+                paragraph: {
+                    class: Paragraph,
+                    inlineToolbar: true,
+                },
+                warning: {
+                    class: Warning,
+                    inlineToolbar: true,
+                }
             },
-           
-            holder: 'editorjs',
-            data:fileData?.document?JSON.parse(fileData.document):rawDocument
-          });
-          ref.current=editor;
+            data: fileData?.document ? JSON.parse(fileData.document) : rawDocument,
+        });
+        ref.current = editor;
     }
 
-    const onSaveDocument=()=>{
-      if(ref.current)
-      {
-        ref.current.save().then((outputData) => {
-          console.log('Article data: ', outputData);
-          updateDocument({
-            _id:fileId,
-            document:JSON.stringify(outputData)
-          }).then(resp=>{
-            
-              toast('Document Updated!')
-            
-          },(e)=>{
-            toast("Server Error!")
-          })
-        }).catch((error) => {
-          console.log('Saving failed: ', error)
-        });
-      }
+    const onSaveDocument = () => {
+        if (ref.current) {
+            ref.current.save().then((outputData: OutputData) => {
+                console.log('Article data: ', outputData);
+                updateDocument({
+                    _id: fileId,
+                    document: JSON.stringify(outputData)
+                }).then(() => {
+                    toast('Document Updated!')
+                }).catch(() => {
+                    toast("Server Error!")
+                });
+            }).catch((error) => {
+                console.log('Saving failed: ', error)
+            });
+        }
     }
-  return (
-    <div>
-        <div id='editorjs' className='ml-20'></div>
-    </div>
-  )
+
+    return (
+        <div>
+            <div id='editorjs' className='ml-20'></div>
+        </div>
+    )
 }
 
-export default Editor
+export default Editor;

419417fbe893bde97c17aefa4a3fdd9ed8660403
ibra-kdbra <git-repo-develop@outlook.com>
2025-02-28 20:02:10 +0300

    banner UI, 03

---
+++ nuxt-clean-architecture/layers/version-03/components/VersionBanner03.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+const { isVisible, version, close } = useVersion()
+</script>
+
+<template>
+  <VersionBannerUI
+    :is-visible="isVisible"
+    :version="version"
+    @close="close"
+  />
+</template>

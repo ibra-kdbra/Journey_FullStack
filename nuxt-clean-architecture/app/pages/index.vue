b6e439c9abe90aafb8daabb339b5171d18c15e89
ibra-kdbra <git-repo-develop@outlook.com>
2025-02-28 20:10:41 +0300

    index page

---
+++ nuxt-clean-architecture/app/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="h-screen grid grid-rows-[auto_1fr]">
+    <header>
+      <VersionBanner01 />
+      <VersionBanner06 />
+    </header>
+    <main class="container mx-auto grid items-center place-items-center">
+      <div class="w-fit border rounded p-8 shadow-lg">
+        <h1
+          class="text-5xl font-bold"
+        >
+          Nuxt Clean Architecture
+        </h1>
+      </div>
+    </main>
+  </div>
+</template>

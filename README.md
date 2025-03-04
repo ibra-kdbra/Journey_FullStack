51f6ebc06ac6df34cec697e0a43004383ba46062
ibra-kdbra <git-repo-develop@outlook.com>
2025-03-04 14:38:59 +0300

    packages, deps, providers, configurations

---
+++ README.md
@@ -0,0 +1,34 @@
+## Project Introduction
+
+This project is a imitation of the [Honor Official Website Homepage](https://www.honor.com/global/magic-os-7/) website.
+
+## Technical Stack
+
+* React
+* Gsap
+* TypeScript
+
+## Run
+
+Start the project
+
+```
+npm install
+```
+
+```
+npm dev
+```
+
+Build the Project
+
+```
+npm build
+```
+
+## Preview
+
+![alt](./preview/home.png)
+
+
+
---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+export default {
+  // Use single quotes for strings
+  singleQuote: true,
+  // Add spaces between object brackets
+  bracketSpacing: true,
+  // No trailing commas in objects
+  trailingComma: 'none',
+  // Remove parentheses around arrow function parameters
+  arrowParens: 'avoid',
+  // Add semicolons at the end of statements
+  semi: true,
+  // Place the ">" of a multi-line HTML element on a new line
+  bracketLine: false,
+  // Use the OS's default line ending
+  endOfLine: 'auto'
+};

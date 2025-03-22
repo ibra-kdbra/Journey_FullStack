b8164c7dc2d31f5faa43fad3f8a84f4b11db2038
ibra-kdbra <git-repo-develop@outlook.com>
2025-03-22 14:36:21 +0300

    stripe, db, uploadthing, utils, queries

---
+++ src/lib/utils.ts
@@ -0,0 +1,16 @@
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function getStripeOAuthLink(
+  accountType: 'agency' | 'subaccount',
+  state: string
+) {
+  const baseUrl = process.env.NEXT_PUBLIC_URL ?? 'https://dominium.netlify.app/subaccount'; // Provide a fallback URL here
+  const redirectUri = `${baseUrl}${baseUrl.endsWith('/') ? '' : '/'}${accountType}`;
+
+  return `https://connect.stripe.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_STRIPE_CLIENT_ID}&scope=read_write&redirect_uri=${redirectUri}&state=${state}`;
+}

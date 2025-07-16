18a4e6a453a8de9634068d5414905342f4e88c3c
ibra-kdbra <git-repo-develop@outlook.com>
2025-07-16 23:28:47 +0300

    three templates

---
+++ sveltekit/src/lib/utils/mail/templates/TeamInvite.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import { Html, Head, Body, Column, Section, Text } from 'sailkit';
+  import { mailTheme } from './mailTheme';
+  interface TeamInviteEmailProps {
+    url: string;
+    invitedBy: string;
+  }
+
+  const { url, invitedBy }: TeamInviteEmailProps = $props();
+</script>
+
+<Html>
+  <Head
+    subject="👥 You're Invited to Collaborate on SvelteKit!"
+    preview="Join your team on SvelteKit"
+    theme={mailTheme}
+  />
+  <Body>
+    <Section>
+      <Column>
+        <Text paddingBottom="0px">
+          <h1>
+            👥 You're Invited to Collaborate on <span style="color: #f59e0b">SvelteKit</span>
+          </h1>
+        </Text>
+        <Text>
+          <p>Hey! 👋</p>
+          <p>{invitedBy} has invited you to join their team on SvelteKit! 🥁</p>
+          <p>To get started, click the link below and accept the invitation:</p>
+          <p>
+            <a href={url}>Click here to join</a>
+          </p>
+        </Text>
+        <Column.Divider />
+        <Text>
+          <p>
+            If you have any questions, feel free to
+            <a href="https://mailto:sonarfarouq@gmail.com">reach out</a>.
+          </p>
+          <p>Welcome aboard! 🎉</p>
+        </Text>
+      </Column>
+    </Section>
+  </Body>
+</Html>
---
+++ sveltekit/src/lib/utils/mail/templates/Welcome.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { Html, Head, Body, Column, Section, Text } from 'sailkit';
+  import { mailTheme } from './mailTheme';
+
+  interface WelcomeEmailProps {
+    userFirstName: string;
+  }
+
+  const { userFirstName }: WelcomeEmailProps = $props();
+</script>
+
+<Html>
+  <Head
+    subject="🥋 Welcome to SvelteKit!"
+    preview="We are thrilled to have you onboard. ⭐"
+    theme={mailTheme}
+  />
+  <Body>
+    <Section>
+      <Column>
+        <Text paddingBottom="0px">
+          <h1>Welcome to <span style="color: #f59e0b">SvelteKit</span></h1>
+        </Text>
+        <Text>
+          <p>Hey, {userFirstName}!</p>
+          <p>We're thrilled to have you onboard. ⭐</p>
+        </Text>
+        <Column.Divider />
+        <Text>
+          <p>
+            Need help? we are just an <a href="https://mailto:sonarfarouq@gmail.com">email</a> away.
+          </p>
+        </Text>
+      </Column>
+    </Section>
+  </Body>
+</Html>

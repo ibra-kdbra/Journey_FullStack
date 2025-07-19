82e3541f01791035d1a6f1f699785769a762a66b
ibra-kdbra <git-repo-develop@outlook.com>
2025-07-19 23:55:51 +0300

    routes layout url

---
+++ sveltekit/src/routes/(auth)/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Wrapper from '$components/Wrapper.svelte';
+  import { Button } from '$components/ui/button';
+  interface Props {
+    children?: import('svelte').Snippet;
+  }
+
+  let { children }: Props = $props();
+</script>
+
+<nav class="fixed w-full p-2">
+  <div>
+    <Button href="/" variant="ghost" class="font-secondary text-lg font-semibold">SvelteKit Omakase</Button>
+  </div>
+</nav>
+
+<div class="flex min-h-screen items-center justify-center">
+  <Wrapper>
+    {@render children?.()}
+  </Wrapper>
+</div>

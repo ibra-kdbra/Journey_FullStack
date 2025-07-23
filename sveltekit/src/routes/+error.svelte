10561f9cc4a11eca9d41b6bdd5bcd4a137c9c212
ibra-kdbra <git-repo-develop@outlook.com>
2025-07-23 23:32:33 +0300

    error svelete page

---
+++ sveltekit/src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script>
+  // Stores
+  import { page } from '$app/state';
+</script>
+
+<div class="flex min-h-screen flex-col items-center justify-center">
+  <h1 class="my-2 text-2xl font-bold md:text-6xl">
+    {page.status}: {page.error?.message}
+  </h1>
+  <p>
+    <a href="/" class="text-muted-foreground/80 hover:text-muted-foreground text-base md:text-xl">Go back home</a>
+  </p>
+</div>

9360898d34c0610d72020529b38f6a7d1924c4ce
ibra-kdbra <git-repo-develop@outlook.com>
2025-10-30 23:54:37 +0300

    Theme switcher, dark__light

---
+++ solid-flask-web-app/ui/src/components/ThemeSwitcher.tsx
@@ -0,0 +1,35 @@
+import { createSignal, createEffect, JSXElement } from 'solid-js'
+
+export function ThemeSwitcher(): JSXElement {
+  const [isDark, setIsDark] = createSignal<boolean>(
+    JSON.parse(localStorage.getItem('isDark') ?? 'false')
+  )
+
+  createEffect(() => {
+    localStorage.setItem('isDark', JSON.stringify(isDark()))
+    document.documentElement.setAttribute(
+      'data-theme',
+      isDark() ? 'dark' : 'light'
+    )
+  })
+
+  return (
+    <label class="swap swap-rotate mx-2">
+      <input
+        type="checkbox"
+        class="theme-controller"
+        checked={isDark()}
+        value="dark"
+        onChange={() => setIsDark(!isDark())}
+      />
+
+      <span class="swap-off fill-current">
+        <i class="fa-regular fa-sun" />
+      </span>
+
+      <span class="swap-on fill-current">
+        <i class="fa-regular fa-moon" />
+      </span>
+    </label>
+  )
+}

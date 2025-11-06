cf520ff4df01512cd9bbbff8f6fb86b2266e46b7
ibra-kdbra <git-repo-develop@outlook.com>
2025-11-06 23:52:49 +0300

    user provider context set

---
+++ solid-flask-web-app/ui/src/context/UserProvider.tsx
@@ -0,0 +1,56 @@
+import {
+  createContext,
+  createSignal,
+  onMount,
+  useContext,
+  Accessor,
+  JSXElement,
+} from 'solid-js'
+
+import { whoAmI } from '../api'
+import { User } from '../models/User'
+
+interface UserContextAttributes {
+  user: Accessor<User | null>
+  setUser: (user: User | null) => void
+  fetchUser: () => Promise<void>
+  loading: Accessor<boolean>
+}
+
+const UserContext = createContext<UserContextAttributes | null>(null)
+
+export const UserProvider = (props: { children: JSXElement }) => {
+  const [user, setUser] = createSignal<User | null>(null)
+  const [loading, setLoading] = createSignal(true)
+
+  const fetchUser = async () => {
+    const response = await whoAmI()
+
+    if (response.status === 200) {
+      setUser(new User(await response.json()))
+    } else {
+      setUser(null)
+    }
+
+    setLoading(false)
+  }
+
+  onMount(() => {
+    fetchUser()
+  })
+
+  return (
+    <UserContext.Provider value={{ user, setUser, loading, fetchUser }}>
+      {props.children}
+    </UserContext.Provider>
+  )
+}
+
+export const useUser = (): UserContextAttributes => {
+  const userContext = useContext(UserContext)
+  if (userContext === null) {
+    throw new Error('useUser can only be used within a UserProvider')
+  }
+
+  return userContext
+}

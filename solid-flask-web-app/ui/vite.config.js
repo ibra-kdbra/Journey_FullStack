158e6271a350a895c836ddef1e5ec11ee0aa7203
ibra-kdbra <git-repo-develop@outlook.com>
2025-11-20 23:50:39 +0300

    confs, others

---
+++ solid-flask-web-app/ui/vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import tailwindcss from "@tailwindcss/vite";
+import solidPlugin from 'vite-plugin-solid';
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    solidPlugin(),
+  ],
+  build: {
+    target: 'esnext',
+    polyfillDynamicImport: false,
+  },
+  server: {
+    port: 5173,
+    cors: true,
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:5000/",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+})

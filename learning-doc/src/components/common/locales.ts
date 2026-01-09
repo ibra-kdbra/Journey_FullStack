813dfb7ca899834ed54ff502abb162ecfda2b791
ibra-kdbra <git-repo-develop@outlook.com>
2026-01-10 00:03:32 +0300

    lang addition

---
+++ learning-doc/src/components/common/locales.ts
@@ -0,0 +1,8 @@
+export const languages = [
+  { code: "en", label: "English", short: "EN" },
+  { code: "zh-hk", label: "Chinese (Cantonese)", short: "HK" },
+  { code: "zh-cn", label: "Chinese (Mandarin)", short: "CN" },
+  { code: "ko", label: "Korean", short: "KO" },
+  { code: "ja", label: "Japanese", short: "JA" },
+  { code: "ms", label: "Malay", short: "MS" },
+];

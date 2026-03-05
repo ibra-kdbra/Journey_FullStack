8b82cf256d5af72c24ac62927afec925ae3bf948
ibra-kdbra <git-repo-develop@outlook.com>
2026-03-06 00:02:25 +0300

    feat(ui/Separator): add visual divider primitive

---
+++ learning-doc/components/ui/Separator.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const props = defineProps<{
+    orientation?: 'horizontal' | 'vertical'
+}>()
+</script>
+
+<template>
+    <div class="shrink-0 bg-slate-200 dark:bg-slate-800"
+        :class="orientation === 'vertical' ? 'h-full w-[1px]' : 'h-[1px] w-full'" />
+</template>

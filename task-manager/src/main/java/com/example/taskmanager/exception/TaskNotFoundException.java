f578067721d5e50878c68d18752de35f7997bc04
ibra-kdbra <git-repo-develop@outlook.com>
2026-05-10 19:35:25 +0300

    removed the trash

--- task-manager/src/main/java/com/example/taskmanager/exception/TaskNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.taskmanager.exception;
-
-public class TaskNotFoundException extends RuntimeException {
-  public TaskNotFoundException(Long id) {
-    super("Task not found: " + id);
-  }
-}
--- task-manager/src/main/java/com/example/taskmanager/exception/UnauthorizedTaskAccessException.java
+++
@@ -1,7 +0,0 @@
-package com.example.taskmanager.exception;
-
-public class UnauthorizedTaskAccessException extends RuntimeException {
-  public UnauthorizedTaskAccessException(Long id) {
-    super("Access denied for task: " + id);
-  }
-}
